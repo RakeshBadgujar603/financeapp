7c3040dbe302f2c9664630deb85bc61456f511f1
RakeshBadgujar603 <rakeshbadgujar603@.com>
2023-12-08 12:38:35 +0530

    initialize project

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AntDesign from "react-native-vector-icons/AntDesign"
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <AntDesign
+      name="forward"
+      size={32}
+      />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
